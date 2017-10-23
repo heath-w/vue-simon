0385128c79fef97723290956a210e92f9313fe07
Heath Williams <awilli01@amgen.com>
2017-10-23 10:30:37 -0400

    Added Event Bus

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+const EventBus = new Vue();
+Object.defineProperties( Vue.prototype, {
+	$bus: {
+		get: function() { return EventBus; }
+	}
+});
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
